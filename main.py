cb5f8c5199ac08367e34e95a753f92099b391d35
marstxa <marstxa@gmail.com>
2025-10-27 23:24:32 +0000

    added all features to the booktop is now able to receive any path of a file from CLI and provide a descriptive data analysis of the characters inside text files

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import sys
 from stats import count_words
 from stats import character_count
+from stats import display_data
 
-def get_book_text(file_path="books/frankenstein.txt"):
-    with open(file_path) as book:
+
+def get_book_text():
+    
+    #get arguments 
+    arguments = sys.argv
+    print(arguments)
+
+    if len(arguments) >= 2:
+        path_to_file = arguments[1] #get path to file
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1) #exit program
+
+    with open(path_to_file) as book:
         file_contents = book.read()
-        return file_contents
+        return file_contents, path_to_file
 
 def main():
-    count_words(get_book_text())
-    character_count(get_book_text())
-
+    book, path = get_book_text()
+    word_count = count_words(book)
+    char_count = character_count(book)
+    display_data(char_count, path, word_count) 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,14 @@
+# Data analysis functions for main.py
+
 def count_words(book):
+    # Get the total number of letters in a book and print the result to the terminal
+    
     list_of_words = book.split()
     count = 0
 
     for num in list_of_words:
         count += 1
     
-    print(f"Found {count} total words")
     return count
 
 def character_count(book):
@@ -20,8 +23,28 @@ def character_count(book):
         else:
             dictionary_of_characters[character] = 1
 
-    print(dictionary_of_characters)
 
     return dictionary_of_characters
 
+def sort_dict(dictionary):
+    sorted_items = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+    return sorted_items
 
+def display_data(dictionary, book, word_count):
+    
+    #Sort dictionary
+    sorted_dictionary = sort_dict(dictionary)
+
+    #Print functions to display data:
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")    
+
+    #print dict keys and values in order
+    for value in sorted_dictionary: 
+        if value.isalpha():
+            print(f"{value}: {sorted_dictionary[value]}") 
+
+    return sorted_dictionary
